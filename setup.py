5763e83c13b7afda4236269629bf02f4599ae142
patrick aubin <paubin12@gmail.com>
2013-03-06 10:24:53 -0600

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='CapBlog',
-    version='0.1.0',
+    version='0.2.0',
     author='Patrick Aubin',
     author_email='patrick@capacitr.com',
     packages=['cap_blog'],
